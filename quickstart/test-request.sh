6fb9dfbb3ae6bd3e92dec652b9fcedaf9e0801a4
Brent Salisbury <bsalisbu@redhat.com>
2025-05-12 19:44:54 -0400

    Update the quickstart validate script to v1/completion (#111)
    - Temporary until the vllm nixl patch lands with v1/chat support.
    
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- quickstart/test-request.sh
+++ quickstart/test-request.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 REPO_ROOT=$(git rev-parse --show-toplevel)
 NAMESPACE=${1:-llm-d}
 
@@ -17,46 +19,46 @@ fi
 
 MODEL_ID=${2:-${MODEL_NAME}}
 
-POD_IP=$(kubectl get pods -n ${NAMESPACE} -o jsonpath='{range .items[*]}{.metadata.name}{" "}{.status.podIP}{"\n"}{end}' | grep decode | awk '{print $2}')
+POD_IP=$(kubectl get pods -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{" "}{.status.podIP}{"\n"}{end}' | grep decode | awk '{print $2}')
 echo "Testing GET /v1/models to ${NAMESPACE} at Pod IP ${POD_IP}"
 RANDOM=$(shuf -i 1-10000 -n 1)
-kubectl run --rm -i curl-${RANDOM} --image=curlimages/curl --restart=Never -- \
+kubectl run --rm -i curl-"${RANDOM}" --image=curlimages/curl --restart=Never -- \
     curl -X GET \
     "http://${POD_IP}:8000/v1/models" \
     -H 'accept: application/json' \
     -H 'Content-Type: application/json'
 
-echo "Testing POST /v1/chat/completions to ${NAMESPACE} at Pod IP ${POD_IP}"
+echo "Testing POST /v1/completions to ${NAMESPACE} at Pod IP ${POD_IP}"
 RANDOM=$(shuf -i 1-10000 -n 1)
-kubectl run --rm -i curl-${RANDOM} --image=curlimages/curl --restart=Never -- \
+kubectl run --rm -i curl-"${RANDOM}" --image=curlimages/curl --restart=Never -- \
   curl -X POST \
-  "http://${POD_IP}:8000/v1/chat/completions" \
+  "http://${POD_IP}:8000/v1/completions" \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-    "model": "'${MODEL_ID}'",
-    "messages": [{"content": "Who are you?", "role": "user"}],
+    "model": "'"${MODEL_ID}"'",
+    "prompt": "Who are you?",
     "stream": false
   }'
 
-GATEWAY_ADDRESS=$(kubectl get gateway -n ${NAMESPACE} | tail -n 1 | awk '{print $3}')
+GATEWAY_ADDRESS=$(kubectl get gateway -n "${NAMESPACE}" | tail -n 1 | awk '{print $3}')
 echo "Testing GET /v1/models to ${NAMESPACE} at Gateway IP ${GATEWAY_ADDRESS}"
 RANDOM=$(shuf -i 1-10000 -n 1)
-kubectl run --rm -i curl-${RANDOM} --image=curlimages/curl --restart=Never -- \
+kubectl run --rm -i curl-"${RANDOM}" --image=curlimages/curl --restart=Never -- \
     curl -X GET \
     "http://${GATEWAY_ADDRESS}/v1/models" \
     -H 'accept: application/json' \
     -H 'Content-Type: application/json'
 
-echo "Testing POST /v1/chat/completions to ${NAMESPACE} at Gateway IP ${GATEWAY_ADDRESS}"
+echo "Testing POST /v1/completions to ${NAMESPACE} at Gateway IP ${GATEWAY_ADDRESS}"
 RANDOM=$(shuf -i 1-10000 -n 1)
-kubectl run --rm -i curl-${RANDOM} --image=curlimages/curl --restart=Never -- \
+kubectl run --rm -i curl-"${RANDOM}" --image=curlimages/curl --restart=Never -- \
   curl -X POST \
-  "http://${GATEWAY_ADDRESS}/v1/chat/completions" \
+  "http://${GATEWAY_ADDRESS}/v1/completions" \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-    "model": "'${MODEL_ID}'",
-    "messages": [{"content": "Who are you?", "role": "user"}],
+    "model": "'"${MODEL_ID}"'",
+    "prompt": "Who are you?",
     "stream": false
   }'
